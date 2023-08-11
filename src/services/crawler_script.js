3f5c2c8fa73f862a17df87471efd389594d46410
Muhammad Lutfi Rahmawan <lutfi.rahmawan@lndata.com>
2023-08-11 17:50:30 +0700

    fix(Crawler): :bug: fix css selector to use `nth-of-type` and refine path summarizer

--- src/services/crawler_script.js
+++ src/services/crawler_script.js
@@ -1,21 +1,49 @@
-export function generateCssSelector(element) {
-  const selectorComponents = [];
+export function generateCssSelector(el) {
+  if (!(el instanceof Element)) return;
 
-  while (element) {
-    if (element.id) {
-      selectorComponents.unshift(`#${element.id}`);
-      break;
-    } else {
-      let selector = element.nodeName.toLowerCase();
-      if (element.className) {
-        selector += `.${element.className.trim().replace(/\s+/g, ".")}`;
+  const parts = [];
+  while (el.nodeType === Node.ELEMENT_NODE) {
+    let tag = el.nodeName.toLowerCase();
+    let elementType = el.tagName
+    let idx = 1
+    let sibling = el.previousElementSibling || null
+    if (sibling) { var hasSibling = el.previousElementSibling.tagName == elementType ? true : false }
+    while (sibling) {
+      if (sibling.tagName === elementType) {
+        idx++;
       }
-      selectorComponents.unshift(selector);
-      element = element.parentElement;
+      sibling = sibling.previousElementSibling;
     }
+
+    /**
+     * Use of nth-of-type is safer than nth-child because will count index based on same siblings with similiar tag
+    */
+
+    let selector = elementType.toLowerCase();
+    selector += `:nth-of-type(${idx})`;
+    parts.unshift(`${selector}`);
+
+    /**
+     * Below code is for selecting whenever element is uniquely having certain selector but quite unsafe
+     * since adjacent elements which doesn't needed are grabbed
+    */
+
+    // if (!hasSibling) {
+    //   let id = el.id ? `#${el.id}` : "";
+    //   let className = el.className
+    //     ? `.${el.className.trim().replace(/\s+/g, ".")}`
+    //     : "";
+    //   parts.unshift(`${tag}${id}${className}`);
+    // }
+    // else {
+    //   let selector = elementType.toLowerCase();
+    //   selector += `:nth-of-type(${idx})`;
+    //   parts.unshift(`${selector}`);
+    // }
+    el = el.parentNode;
   }
 
-  return selectorComponents.join(" > ");
+  return parts.join(" > ");
 }
 
 export function summarizeCSSPaths(path1, path2) {
@@ -27,20 +55,34 @@ export function summarizeCSSPaths(path1, path2) {
   for (let i = 0; i < Math.min(segments1.length, segments2.length); i++) {
     let first = segments1[i];
     let second = segments2[i];
-    if (first.split(".").length > 1 && first === second) {
-      if (first.includes(".highlightEl")) {
-        first = first.replace(".highlightEl", "");
-        segments1[i] = first.split(".").slice(0, -1).join(".");
-      } else {
-        segments1[i] = first;
+
+    if (first === second) {
+      commonSelector += first + " > ";
+    }
+    else if (first.includes(".") && second.includes(".")) {
+      const selectors1 = first.split(".")
+      const selectors2 = second.split(".")
+
+      for (let i = 0; i < Math.min(selectors1.length, selectors2.length); i++) {
+        let sel1 = selectors1[i]
+        let sel2 = selectors2[i]
+        if (sel1 !== sel2) {
+          commonSelector += selectors1.slice(0, i).join(".") + " > "
+
+        }
       }
     }
-    if (document.querySelectorAll(segments1.join(" > ")).length === 1) {
-      segments1[i] = first.split(".")[0];
+    else {
+      const regex = /^\w+(?=[\.\:])/g
+      tag = first.match(regex).pop()
+      commonSelector += tag + " > ";
     }
   }
 
-  return segments1.join(" > "); // Remove the trailing ' > '
+  // replace the injected CSS class by our extension
+  commonSelector = commonSelector.replace(".highlightEl", "")
+
+  return commonSelector.slice(0, -3);
 }
 
 export function extract_data(elements) {
